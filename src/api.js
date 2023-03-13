1f7ba271dca9b8577aef9745d0d1f0966cb850c2
Islam hassan <37153826+islamhassan1@users.noreply.github.com>
2023-03-13 14:18:13 +0200

    add

--- src/api.js
+++ src/api.js
@@ -4,14 +4,14 @@ const API_KEY = "8366825ffff4d2cffc06f418a4ac78cb";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast";
 
-export const fetchWeatherData = async (lat, lon) => {
-  const url = `${API_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+export const fetchWeatherData = async (city) => {
+  const url = `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await axios.get(url);
   return response.data;
 };
 
-export const fetchWeatheForecast = async (lat, lon) => {
-  const urlFor = `${API_URL_FORECAST}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+export const fetchWeatheForecast = async (city) => {
+  const urlFor = `${API_URL_FORECAST}?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await axios.get(urlFor);
   return response.data;
 };
